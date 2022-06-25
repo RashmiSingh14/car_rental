86e4dd9036c6c80fd2e1cf272056da04cb49796f
Rashmi Singh <rashmi.virtualgain5@gmail.com>
2022-06-25 11:43:24 +0530

    WIP: working on models

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,17 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/database")
 
 //set config file
-dotenv.config({path:"backend/config/config.env"});
+dotenv.config({ path: "backend/config/config.env" });
 
 //connect to db
 connectDB()
 
 
-// app.listen(process.env.PORT, () => {
-//     console.log("Server is running on http://localhost:${process.env.PORT}");
-// });
-
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on http://localhost:", process.env.PORT);
 });
+
+
+// app.listen(3000, () => {
+//     console.log("Server is running on http://localhost:3000");
+// });
